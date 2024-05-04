dbbf702f5489d271bc67c5b673faeda3c859cde7
marina.martin.sanchez <marina.martin.sanchez@estudiantat.upc.edu>
2024-05-04 19:30:48 +0200

    message

--- env/app.py
+++ env/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for 
+from flask import Flask, request
 from computeDistanceFromRouterStrength import computeDistanceFromRouter1Strength, computeDistanceFromRouter2Strength, computeDistanceFromRouter3Strength
 from triangulatePosition import triangulatePosition
 import psycopg2 
