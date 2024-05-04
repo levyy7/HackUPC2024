f17f41f4262675e8187bd74f88a19cb2e324b01d
marina.martin.sanchez <marina.martin.sanchez@estudiantat.upc.edu>
2024-05-04 19:15:16 +0200

    from flask import Flask, render_template, request, redirect, url_for

--- env/app.py
+++ env/app.py
@@ -1,8 +1,30 @@
-from flask import Flask, request
+from flask import Flask, render_template, request, redirect, url_for 
 from computeDistanceFromRouterStrength import computeDistanceFromRouter1Strength, computeDistanceFromRouter2Strength, computeDistanceFromRouter3Strength
 from triangulatePosition import triangulatePosition
+import psycopg2 
 
 app = Flask(__name__)
+connection = psycopg2.connect(database="hack2024_vueling", user="postgres",
+                              password="psql", host="localhost", port="5432")
+cursor = connection.cursor()
+
+#crear la tabla en la base de datos si no existia
+cursor.execute(
+    '''CREATE TABLE IF NOT EXISTS ticket(
+        id varchar(8) PRIMARY KEY,
+        boarding_gate int NOT NULL);'''
+)
+#inicializar la tabla
+#cursor.execute(
+#    '''INSERT INTO ticket (id, boarding_gate) VALUES \
+#        ('12345678', 0),
+#        ('abcdefgh', 1),
+#        ('1b3d5f7h', 2);
+#        '''
+#)
+connection.commit()
+cursor.close()
+connection.close()
 
 r1X, r1Y = 0, 0
 r2X, r2Y = 1, 0
@@ -39,3 +61,25 @@ def data_get():
         "posX": posX, 
         "posY": posY
     }
+
+#http://192.168.137.1:5000/test2?ticket_id=12345678
+@app.get("/test2")
+def data_get2():
+    ticket_id = request.args.get('ticket_id')
+    connection = psycopg2.connect(database="hack2024_vueling", 
+                            user="postgres", 
+                            password="psql", 
+                            host="localhost", port="5432") 
+    cursor = connection.cursor()
+    
+    cursor.execute('''SELECT boarding_gate
+                   FROM ticket
+                   WHERE id = ''' + "'" + ticket_id + "'")
+    boarding_gate = cursor.fetchall()[0][0]
+    
+    cursor.close()
+    connection.close()
+    
+    return str(boarding_gate)
+    
+    
